898c5016339fa13c133b11f6a2f3254c3fc038f9
bhargav1907 <30969725+bhargav1907@users.noreply.github.com>
2022-07-30 13:23:32 -0400

    Final update to the webapp.

--- Dashboard/Model_webapp.py
+++ Dashboard/Model_webapp.py
@@ -74,6 +74,7 @@ df = pd.DataFrame(get_jobs_from_prefix(BUCKET_NAME, '2022/07/'))
 # removing the rows with no job description
 df = df.drop(['salary_estimated'], axis = 1)
 df.dropna(inplace=True)
+df.drop_duplicates(keep = False, inplace = True)
 df = df.reset_index(drop=True)
 
 # Cleaning Job description data
@@ -86,6 +87,7 @@ df['job_description'] = df['job_description'].apply(lambda x: re.sub(' +',' ',x)
 # Stop word removal
 stop_words = set(stopwords.words('english'))
 df['job_description'] = df['job_description'].apply(lambda x: ' '.join(w for w in word_tokenize(x) if not w.lower() in stop_words))
+
 
 if fl:
     with open(os.path.join(os.getcwd(),fl.name),"wb") as f:
@@ -135,57 +137,66 @@ if fl:
     
     res['url'] = res['url'].apply(lambda x:  f'<a target="_blank" href="{x}">Job URL</a>')
 
-    col1, col2, col3, col4, col5 = st.columns(5)
+    col2, col3, col4, col5 = st.columns(4)
 
-    with col1:
-        st.markdown("<h2 style='text-align: center;'>No</h2>", unsafe_allow_html=True)
-        # for i in range(10):
-        st.markdown("<h5 style='text-align: center;'>1</h5>", unsafe_allow_html=True)
-        st.write("")
-        st.markdown("<h5 style='text-align: center;'>2</h5>", unsafe_allow_html=True)
-        st.write("")
-        st.markdown("<h5 style='text-align: center;'>3</h5>", unsafe_allow_html=True)
-        st.write("")
-        st.markdown("<h5 style='text-align: center;'>4</h5>", unsafe_allow_html=True)
-        st.write("")
-        st.markdown("<h5 style='text-align: center;'>5</h5>", unsafe_allow_html=True)
-        st.write("")
-        st.markdown("<h5 style='text-align: center;'>6</h5>", unsafe_allow_html=True)
-        st.write("")
-        st.markdown("<h5 style='text-align: center;'>7</h5>", unsafe_allow_html=True)
-        st.write("")
-        st.markdown("<h5 style='text-align: center;'>8</h5>", unsafe_allow_html=True)
-        st.write("")
-        st.markdown("<h5 style='text-align: center;'>9</h5>", unsafe_allow_html=True)
-        st.write("")
-        st.markdown("<h5 style='text-align: center;'>10</h5>", unsafe_allow_html=True)
-        # st.write(str(i))
+    # with col1:
+    #     st.markdown("<h2 style='text-align: center;'>No</h2>", unsafe_allow_html=True)
+    #     # for i in range(10):
+    #     st.markdown("<h5 style='text-align: center;'>1</h5>", unsafe_allow_html=True)
+    #     st.write("")
+    #     st.markdown("<h5 style='text-align: center;'>2</h5>", unsafe_allow_html=True)
+    #     st.write("")
+    #     st.markdown("<h5 style='text-align: center;'>3</h5>", unsafe_allow_html=True)
+    #     st.write("")
+    #     st.markdown("<h5 style='text-align: center;'>4</h5>", unsafe_allow_html=True)
+    #     st.write("")
+    #     st.markdown("<h5 style='text-align: center;'>5</h5>", unsafe_allow_html=True)
+    #     st.write("")
+    #     st.markdown("<h5 style='text-align: center;'>6</h5>", unsafe_allow_html=True)
+    #     st.write("")
+    #     st.markdown("<h5 style='text-align: center;'>7</h5>", unsafe_allow_html=True)
+    #     st.write("")
+    #     st.markdown("<h5 style='text-align: center;'>8</h5>", unsafe_allow_html=True)
+    #     st.write("")
+    #     st.markdown("<h5 style='text-align: center;'>9</h5>", unsafe_allow_html=True)
+    #     st.write("")
+    #     st.markdown("<h5 style='text-align: center;'>10</h5>", unsafe_allow_html=True)
+    #     # st.write(str(i))
 
     with col2:
         st.header("Company")
         for i in range(10):
-            st.write(res.iloc[i,0])
-            st.write("")
+            # st.write(res.iloc[i,0])
+            st.markdown(res.iloc[i,0])
+            st.markdown("")
+            # st.write("")
 
     with col3:
         st.header("Role")
         for i in range(10):
-            st.write(res.iloc[i,1])
-            st.write("")
+            # st.write(res.iloc[i,1])
+            rl = res.iloc[i,1].split()
+            rl = (" ").join(rl[:5])
+            # st.markdown(res.iloc[i,1])
+            st.markdown(rl)
+            # if len(res.iloc[i,1]) <= 40:
+            st.markdown("")
+            # st.write("  ")
 
     with col4:
         st.header("Location")
         for i in range(10):
-            st.write(res.iloc[i,2])
-            st.write("")       
+            # st.write(res.iloc[i,2])
+            st.markdown(res.iloc[i,2])
+            st.markdown("")
+            # st.write("")       
 
     with col5:
         st.header("URL")
         for i in range(10):
             st.markdown(res.iloc[i,3],unsafe_allow_html=True)
-            st.write("")
-            # st.write(f" [Job URL]({res.iloc[i,3]})")
-            # st.write(res.iloc[i,3])
+            st.markdown("")
+            # st.write("")
     
 
 
@@ -196,70 +207,70 @@ if fl:
        
     # st.table(res)
     
-    # st.write(res.to_html(escape=False, index=False), unsafe_allow_html=True)     
+    # st.write(res.to_html(escape=False, index=True, header=True), unsafe_allow_html=True)     
 
 
 #############################
 # EDA
 #############################
 
-st.header("*EDA*")
+# st.header("*EDA*")
 
-stop_words = set(stopwords.words('english'))
+# stop_words = set(stopwords.words('english'))
 
-# Creating Document Term Matrix
-import sklearn
-from sklearn.feature_extraction.text import CountVectorizer
-cv=CountVectorizer(analyzer='word')
-data=cv.fit_transform(df['job_description'])
-df_dtm = pd.DataFrame(data.toarray(), columns=cv.get_feature_names())
-df_dtm.index = df.index
+# # Creating Document Term Matrix
+# import sklearn
+# from sklearn.feature_extraction.text import CountVectorizer
+# cv=CountVectorizer(analyzer='word')
+# data=cv.fit_transform(df['job_description'])
+# df_dtm = pd.DataFrame(data.toarray(), columns=cv.get_feature_names())
+# df_dtm.index = df.index
 
-# Importing wordcloud for plotting word clouds and textwrap for wrapping longer text
-from wordcloud import WordCloud
-from textwrap import wrap
+# # Importing wordcloud for plotting word clouds and textwrap for wrapping longer text
+# from wordcloud import WordCloud
+# from textwrap import wrap
 
-# Function for generating word clouds
-def plotwordcloud(data,title):
-  wc = WordCloud(width=400, height=330, max_words=150,colormap="Dark2").generate_from_frequencies(data)
-  fig = plt.figure(figsize=(10,8))
-  plt.imshow(wc, interpolation='bilinear')
-  plt.axis("off")
-  plt.title('\n'.join(wrap(title,60)),fontsize=13)
-  plt.show()
-  st.balloons()
-  st.pyplot(fig)
+# # Function for generating word clouds
+# def plotwordcloud(data,title):
+#   wc = WordCloud(width=400, height=330, max_words=150,colormap="Dark2").generate_from_frequencies(data)
+#   fig = plt.figure(figsize=(10,8))
+#   plt.imshow(wc, interpolation='bilinear')
+#   plt.axis("off")
+#   plt.title('\n'.join(wrap(title,60)),fontsize=13)
+#   plt.show()
+#   st.balloons()
+#   st.pyplot(fig)
   
 
-st.subheader("*Popular Jobs*")
-# No of jobs plot
-g1 = dict(df['role'].value_counts()[0:19])
+# st.subheader("*Popular Jobs*")
+# # No of jobs plot
+# g1 = dict(df['role'].value_counts()[0:19])
 
-fig = plt.figure(figsize =(10, 7))
-plt.barh(list(g1.keys()), g1.values())
-plt.title('Popular Data Jobs')
-plt.xlabel('No of jobs')
-plt.ylabel('Jobs')
-plt.show()
+# fig = plt.figure(figsize =(10, 7))
+# plt.barh(list(g1.keys()), g1.values())
+# plt.title('Popular Data Jobs')
+# plt.xlabel('No of jobs')
+# plt.ylabel('Jobs')
+# plt.show()
 
-st.balloons()
-st.pyplot(fig)
+# st.balloons()
+# st.pyplot(fig)
 
-# Word clouds
-st.subheader("*Wordclouds of job descriptions of top 5 jobs*")
+# # Word clouds
+# st.subheader("*Wordclouds of job descriptions of top 5 jobs*")
 
-lemdf = df[['role','job_description']].groupby(by='role').agg(lambda x:' '.join(x)).loc[list(g1.keys())[0:5]]
+# lemdf = df[['role','job_description']].groupby(by='role').agg(lambda x:' '.join(x)).loc[list(g1.keys())[0:5]]
 
-lemdf['job_description'] = lemdf['job_description'].apply(lambda x: ' '.join(w for w in word_tokenize(x) if not w.lower() in stop_words))
+# lemdf['job_description'] = lemdf['job_description'].apply(lambda x: ' '.join(w for w in word_tokenize(x) if not w.lower() in stop_words))
 
-data=cv.fit_transform(lemdf['job_description'])
-df_dtm = pd.DataFrame(data.toarray(), columns=cv.get_feature_names())
-df_dtm.index = lemdf.index
-# df_dtm
+# data=cv.fit_transform(lemdf['job_description'])
+# df_dtm = pd.DataFrame(data.toarray(), columns=cv.get_feature_names())
+# df_dtm.index = lemdf.index
+# # df_dtm
 
-# Transposing document term matrix
-df_dtm=df_dtm.transpose()
+# # Transposing document term matrix
+# df_dtm=df_dtm.transpose()
 
-# Plotting word cloud for each product
-for index,product in enumerate(df_dtm.columns):
-  plotwordcloud(df_dtm[product].sort_values(ascending=False),product)
+# # Plotting word cloud for each product
+# for index,product in enumerate(df_dtm.columns):
+#   plotwordcloud(df_dtm[product].sort_values(ascending=False),product)
